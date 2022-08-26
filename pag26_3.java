6d3b1fc1470131e8effd49221f00fe1a97e98f14
LacosteCarr <104365664+LacosteCarr@users.noreply.github.com>
2022-08-26 11:45:37 -0300

    Add files via upload

---
+++ pag26_3.java
@@ -0,0 +1,12 @@
+public class pag26_3 {
+    public static void main(String[] args) {
+        int n = 123;
+        for (int i = 0; i <= n; i++) {
+            if ((i % 2 != 0) && (i % 3 != 0)) {
+                System.out.println(i);
+            }
+
+        }
+    }
+
+}
