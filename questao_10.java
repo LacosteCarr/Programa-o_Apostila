be1a551603eebbf5a97e30c7e1c06d73042d0935
LacosteCarr <104365664+LacosteCarr@users.noreply.github.com>
2022-08-26 11:53:06 -0300

    Add files via upload

---
+++ questao_10.java
@@ -0,0 +1,14 @@
+    import java.util.Scanner;
+    public class questao_10 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        Integer n1,ant1;
+
+        System.out.print("Digite um número: ");
+        n1 = entrada.nextInt();
+     ant1 = n1-1;
+     System.out.println("O antecessor de "+ n1 +"é: "+ ant1);
+
+    }
+
+  } 
